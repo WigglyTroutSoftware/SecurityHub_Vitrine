0ae47e80def81332dfc3e5ecd1e0e78428b0eb1b
Florian <86839555+florian-tamborine@users.noreply.github.com>
2021-10-21 01:45:44 +0100

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -2,23 +2,25 @@
 echo "Let's go :"
 
 # Variable
+container_name='securityhub'
 this_path=`pwd`
-version_img="1cc740a"
+name_img="securityhub/securityhubrepo"
+version_img="salmon"
 
 # Docker pull images
-if [[ -z `docker images | grep securityhub/securityhubrepo` ]] && [[ `docker images | grep securityhub/securityhubrepo | awk '{print $2}'` == $version_img ]]
+if [[ -z `docker images | grep $name_img` ]] && [[ `docker images | grep $name_img | awk '{print $2}'` == $version_img ]]
 then
-	docker pull securityhub/securityhubrepo:$version_img
-fi
-
-# Docker create volume
-if [[ -z `docker volume ls | grep securityhub_conf` ]]
-then
-	docker volume create --driver local --opt o=bind --opt type=none --opt device=$this_path/securityhub_conf securityhub_conf
+	docker pull $name_img:$version_img
 fi
 
 # Start the container
 echo "--> Start SecurityHub container"
-docker run -d --name securityhub --mount source=securityhub_conf,target=/opt/zeppelin/conf -p 8080:8080 securityhub/securityhubrepo:$version_img
+docker run -d --name $container_name --mount source=securityhub_conf,target=/opt/zeppelin/conf -p 8080:8080 $name_img:$version_img
 
-echo "Security Hub is live"
+# Change uid and gi of snatch user container
+echo "--> Set user"
+uid_start_user=`id -u`
+gid_start_user=`id -g`
+docker exec -u root `docker ps --filter name=$container_name -q` usermod -u $uid_start_user -g $gid_start_user snatch
+
+echo "Security Hub is live on port 8080!"
